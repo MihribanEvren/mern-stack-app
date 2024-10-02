1e9dffa9dcff0ecdf4daff89cd0af7c04a466cd7
Mihriban Evren <mihribanevrenn@gmail.com>
2024-10-02 09:40:39 +0300

    feat: Set up Passport.js local strategy for user authentication.

--- server/strategies/local-strategy.js
+++ server/strategies/local-strategy.js
@@ -1,6 +1,21 @@
 import passport from 'passport';
 import { Strategy } from 'passport-local';
 import { User } from '../models/Users';
+import { comparePassword } from '../utils/helpers';
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const findUser = await User.findById(id);
+    if (!findUser) throw new Error('User Not Found');
+    done(null, findUser);
+  } catch (error) {
+    done(error, null);
+  }
+});
 
 export default passport.use(
   new Strategy(async (username, password, done) => {
